b18c320d9296493dde0173622101bb40dea7d679
Esteban <iarzaesteban94>
2025-03-12 01:42:24 -0300

    Configuramos forntend y probamos conexion con backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import engine, Base
 
 app = FastAPI()
+
+# Habilitar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Cambiar esto en producción
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
@@ -10,5 +20,5 @@ def read_root():
     return {"message": "API is running!"}
 
 @app.get("/home")
-def read_root():
+def read_home():
     return {"message": "Welcome to home page baby!"}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+  const fetchData = async () => {
+    let retries = 5;
+    while (retries) {
+      try {
+        const response = await fetch("http://trueque_backend:8000/home");
+        const data = await response.json();
+        setMessage(data.message);
+        break;
+      } catch (error) {
+        console.error("Error fetching:", error);
+        retries -= 1;
+        await new Promise(resolve => setTimeout(resolve, 2000)); // Esperar 2 segundos antes de reintentar
+      }
+    }
+  };
+
+  fetchData();
+}, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{message}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
